628225288ce7398af41d11e9812ad8da002fc9e0
freedom.yi <851989962@qq.com>
2022-08-24 23:02:15 +0800

    first commit -1

--- app/routes/__main.tsx
+++ app/routes/__main.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import { Outlet } from 'react-router';
+import { useLoaderData } from '@remix-run/react';
+
+export async function loader() {
+  return {
+    NODE_ENV_1: process.env.NODE_DEMO_1
+  }
+}
 
 export default function main() {
+  const data = useLoaderData();
+  console.log(data.NODE_ENV_1);
   return (
     <div>
+      <h2> {process.env.NODE_ENV}</h2>
+      <h3>{data.NODE_ENV_1}</h3>
       <h1>
         主页
       </h1>
